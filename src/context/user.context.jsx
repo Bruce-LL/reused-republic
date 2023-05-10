8c1b0ecd266a81ba116f592e27c7a8d2bd55dc97
xianweilong <longxianwei@hotmail.com>
2023-05-09 23:38:03 -0400

    section 14 completed

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,15 +1,46 @@
-import { createContext, useState, useEffect} from "react";
+import { createContext, useState, useEffect, useReducer} from "react";
 
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
+import  createAction  from "../utils/reducer/reducer.utils";
 // inside, is the actural value you want to acess
 export const UserContext = createContext({
     currentUser: null, 
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+}
+
+const userReducer = (state, action) => {
+    console.log('dispatched');
+    console.log(action);
+    const {type, payload} = action;
+    switch(type){
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+        default: 
+            throw new Error(`unhandled type ${type} in userReducer`);
+    } 
+}
+
 export const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    //const [currentUser, setCurrentUser] = useState(null);
+    const [{currentUser}, dispatcher] = useReducer(userReducer, INITIAL_STATE);
+    console.log(currentUser);
+
+    const setCurrentUser = (user) => {
+        dispatcher(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+    }
+
     const value = {currentUser, setCurrentUser};
 
     useEffect(() => {  //hook for side effect, which means it can happen outside this component
